7f3fca5a1f0a277b7b5e4bb2b4373c367d3a7437
A5H-git <alvn.hendricks@gmail.com>
2025-08-21 14:36:55 -0700

    Updated navbar styling

--- frontend/app/src/components/Navbar/Navbar.tsx
+++ frontend/app/src/components/Navbar/Navbar.tsx
@@ -7,36 +7,45 @@ import { ButtonIcon } from "../ButtonIcon";
 
 function Navbar() {
   return (
-    <div className="flex gap-4 justify-between items-center px-4 py-3 h-16">
+    <div className="flex gap-6 justify-between items-center my-4 px-2 py-4 h-16">
       
       {/* Logo */}
       <Link to="/" className="flex items-center gap-6">
-        <img src="/favicon.png" alt="logo" className="h-16 w-24 object-contain"  />
-        <span className="font-semibold">Alvin Hendricks</span>
+        <img 
+          src="/favicon.png" 
+          alt="logo" 
+          className="h-32 w-32 object-contain"  
+        />
+        <span className="font-semibold hidden sm:inline">
+          Alvin Hendricks
+        </span>
       </Link>
 
       {/* Navigation */}
-      <nav className="flex items-center gap-4">
-        <Link to="/resume" className="hover:text-accent">
-          Resume
-        </Link>
-        <Link to="/projects" className="hover:text-accent">
-          Projects
-        </Link>
-        <Link to="/life" className="hover:text-accent">
-          Life
-        </Link>
+      <nav className="flex flex-3  justify-end items-center">
+        <div className="flex gap-6">
+          <Link to="/projects" className="hover:text-accent">
+            Projects
+          </Link>
+          <Link to="/resume" className="hover:text-accent">
+            Resume
+          </Link>
+          <Link to="/life" className="hover:text-accent">
+            Life
+          </Link>
+        </div>
+
+        <div className="flex px-3">
+          <ButtonIcon link={GITHUB_URL} ariaLabel="GitHub Profile">
+            <GitHubIcon />
+          </ButtonIcon>
+          <ButtonIcon link={LINKEDIN_URL} ariaLabel="LinkedIn Profile">
+            <LinkedInIcon />
+          </ButtonIcon>
+        </div>
       </nav>
 
       {/* Socials */}
-      <div className="flex flex-1 items-center justify-end gap-2">
-        <ButtonIcon link={GITHUB_URL} ariaLabel="GitHub Profile">
-          <GitHubIcon />
-        </ButtonIcon>
-        <ButtonIcon link={LINKEDIN_URL} ariaLabel="LinkedIn Profile">
-          <LinkedInIcon />
-        </ButtonIcon>
-      </div>
     </div>
   );
 }
