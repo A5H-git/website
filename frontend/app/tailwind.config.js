6358e23281e869071423ed87428a38f3ffbf012f
A5H-git <alvn.hendricks@gmail.com>
2025-08-21 17:18:27 -0700

    Updated packages for markdown rendering/styling

--- frontend/app/tailwind.config.js
+++ frontend/app/tailwind.config.js
@@ -56,6 +56,6 @@ export default {
   	}
   },
   // eslint-disable-next-line no-undef
-  plugins: [require("tailwindcss-animate")],
+  plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
 }
 
