86a0b0a078605e1967b94b7ed39875866da6ada5
A5H-git <alvn.hendricks@gmail.com>
2025-08-21 17:19:15 -0700

    Navbar and Footer stays at top/bottom of page

--- frontend/app/src/routes/Root.tsx
+++ frontend/app/src/routes/Root.tsx
@@ -3,19 +3,21 @@ import React from "react";
 import Footer from "@/components/Footer/Footer";
 import Navbar from "@/components/Navbar/Navbar";
 import { Outlet } from "react-router-dom";
-// import Footer from "../components/Footer/index.jsx";
-// import Navbar from "../components/Navbar";
 
 function Root() {
   return (
-    <div className="page-container">
-      <header className="sticky top-00 z-5 p-4">
+    <div className="page-container flex min-h-screen flex-col">
+      <header className="p-4">
         <Navbar />
       </header>
+
       <main>
         <Outlet />
       </main>
-      <Footer />
+
+      <footer className="mt-auto">
+        <Footer />
+      </footer>
     </div>
   );
 }
