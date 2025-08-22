000fdb056b5df82d3c0b2e01ec000f2c85e67735
A5H-git <alvn.hendricks@gmail.com>
2025-08-21 17:19:45 -0700

    Updated markdown rendering to be compatible with GFM

--- frontend/app/src/routes/ProjectDetailPage.tsx
+++ frontend/app/src/routes/ProjectDetailPage.tsx
@@ -1,17 +1,30 @@
 import React from "react";
-
-import ReactMarkdown from "react-markdown";
 import { useLoaderData } from "react-router-dom";
 
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
+
+import { Project } from "@/types/project";
+
+
 const ProjectDetailPage = () => {
-  const project = useLoaderData();
+  const project = useLoaderData<Project>();
 
   return (
-    <>
-      <h1>{project.title}</h1>
-      <image src={project.preview_image}></image>
-      <ReactMarkdown>{project.body}</ReactMarkdown>
-    </>
+    <div className="px-12">
+      <header>
+        <img 
+          src={project.previewImage}
+        />
+        <h1>{project.title}</h1>
+      </header>
+
+      <body className="prose dark:prose-invert max-w-none">
+        <ReactMarkdown remarkPlugins={[remarkGfm]}>
+          {project.body}
+        </ReactMarkdown>
+      </body>
+    </div>
   );
 };
 
