7beadf9da70aff24a1ceed158799e167a65a8bc0
A5H-git <alvn.hendricks@gmail.com>
2025-08-21 19:14:44 -0700

    Set up basic rendering

--- frontend/app/src/routes/ProjectDetailPage.tsx
+++ frontend/app/src/routes/ProjectDetailPage.tsx
@@ -5,25 +5,29 @@ import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 
 import { Project } from "@/types/project";
+import { MEDIA_ROOT } from "@/utils/constants";
 
 
 const ProjectDetailPage = () => {
   const project = useLoaderData<Project>();
+  const previewImage = (
+    project.previewImage
+      ? `${MEDIA_ROOT}${project.previewImage}`
+      : ""
+  );
 
   return (
-    <div className="px-12">
-      <header>
-        <img 
-          src={project.previewImage}
-        />
-        <h1>{project.title}</h1>
+    <div className="flex flex-col px-12 gap-y-4">
+      <header className="flex flex-col items-center text-center gap-y-12">
+        <img src={previewImage}/>
+        <h1 className="text-4xl font-black">{project.title}</h1>
+        <hr className="w-1/2"/>
       </header>
-
-      <body className="prose dark:prose-invert max-w-none">
+      <div className="prose dark:prose-invert max-w-none pt-4">
         <ReactMarkdown remarkPlugins={[remarkGfm]}>
           {project.body}
         </ReactMarkdown>
-      </body>
+      </div>
     </div>
   );
 };
