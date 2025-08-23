3375fb06c5abb54caa5787ee1cfc7dcd9fc8c7f2
A5H-git <alvn.hendricks@gmail.com>
2025-08-22 17:47:24 -0700

    Updated body to separate url and text fields

--- backend/projects/models.py
+++ backend/projects/models.py
@@ -1,18 +1,23 @@
 from django.db import models
 
+from backend.settings import MEDIA_ROOT
+
 
 class Project(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(unique=True, blank=True, null=True)
-    body = models.TextField(default="")
     summary = models.TextField(default="")
 
-    date_created = models.DateTimeField(auto_now_add=True)
-    date_updated = models.DateField(auto_now_add=True)
+    body_text = models.TextField(default="")
+
+    body_url = models.FileField(upload_to="project_files", blank=True, null=True)
 
     preview_image = models.ImageField(
         upload_to="project_previews", blank=True, null=True
     )
+
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateField(auto_now_add=True)
 
     def __str__(self) -> str:
         return self.title
--- backend/projects/serializer.py
+++ backend/projects/serializer.py
@@ -6,4 +6,15 @@ from .models import Project
 class ProjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Project
-        fields = "__all__"
+
+        fields = [
+            "id",
+            "title",
+            "slug",
+            "summary",
+            "body_url",
+            "body_text",
+            "preview_image",
+            "date_created",
+            "date_updated",
+        ]
